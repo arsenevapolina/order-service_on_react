3f889c92e8f600f9ed20a800761378e3bbc603f3
Полина <pechenkova.polina.mgu@yandex.ru>
2024-11-12 01:58:33 +0300

    pizza-app

---
+++ pizza-app/src/main.tsx
@@ -0,0 +1,38 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { RouterProvider, createBrowserRouter  } from 'react-router-dom';
+import { Menu } from "./pages/Menu/Menu";
+import { Cart } from "./pages/Cart/Cart";
+import { Error } from "./pages/Error/Error";
+import { Layout } from "./layout/Menu/Menu";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Menu />,
+      },
+      {
+        path: '/cart',
+        element: <Cart/>
+      }
+    ]
+  },
+  {
+    path: "*",
+    element: <Error />,
+  },
+]);
+
+
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
