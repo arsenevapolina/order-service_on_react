02157e40b46cdbf036e0d840ebc480005b5232fa
Полина <pechenkova.polina.mgu@yandex.ru>
2024-12-02 22:06:33 +0300

    Реализация defer и await

--- pizza-app/src/main.tsx
+++ pizza-app/src/main.tsx
@@ -1,17 +1,16 @@
-import { StrictMode } from "react";
+import { StrictMode, lazy, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { RouterProvider, createBrowserRouter, defer } from "react-router-dom";
 import { Cart } from "./pages/Cart/Cart";
 import { Error as ErrorPage } from "./pages/Error/Error";
 import { Layout } from "./layout/Layout/Layout";
 import { Product } from "./pages/Product/Product";
 import axios from "axios";
 import { PREFIX } from "./helpers/API";
-import { lazy } from 'react';
-import { Suspense } from "react";
 
-const Menu = lazy(() => import('./pages/Menu/Menu'))
+
+const Menu = lazy(() => import('./pages/Menu/Menu'));
 
 const router = createBrowserRouter([
   {
@@ -35,13 +34,14 @@ const router = createBrowserRouter([
         element: <Product />,
         errorElement: <>Ошибка</>,
         loader: async ({ params }) => {
-          await new Promise<void>((resolve) => {
-            setTimeout(() => {
-              resolve();
-            }, 2000);
+          return defer({
+            data: new Promise((resolve, reject) => {
+              setTimeout(() => {
+                axios.get(`${PREFIX}/products/${params.id}`).then(data => resolve(data)).catch(e => reject(e))
+              }, 2000);
+            }
+          )
           });
-          const { data } = await axios.get(`${PREFIX}/products/${params.id}`);
-          return data;
         },
       },
     ],
--- pizza-app/src/pages/Product/Product.tsx
+++ pizza-app/src/pages/Product/Product.tsx
@@ -1,8 +1,22 @@
-import { useLoaderData } from "react-router-dom";
+import { Await, useLoaderData } from "react-router-dom";
 import { IProduct } from '../../interfaces/product.interface'
+import { Suspense } from "react";
 
 export function Product() {
-  const data = useLoaderData() as IProduct;
+  const data = useLoaderData() as { data: IProduct };
 
-  return <>Product - {data.name}</>;
+  return (
+    <>
+      <Suspense fallback={'Загружаю...'}>
+        <Await 
+        resolve={data.data}>
+          {({ data }: { data: IProduct }) => ( 
+            <>IProduct - {data.name}</>
+            )}
+        </Await>
+      </Suspense>
+    </>
+  );
 }
+
+
