8cfe6bbad826a88dc93bcb62e39f8d31cd4f7710
Полина <pechenkova.polina.mgu@yandex.ru>
2024-12-05 20:12:20 +0300

    Возвращены необходимые файлы

--- pizza-app/src/layout/Layout/Layout.tsx
+++ pizza-app/src/layout/Layout/Layout.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { NavLink, Outlet } from "react-router-dom";
 import styles from './Layout.module.css';
 import Button from "../../components/Button/Button";
--- pizza-app/src/main.tsx
+++ pizza-app/src/main.tsx
@@ -1,3 +1,4 @@
+import React from "react"; 
 import { StrictMode, lazy, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
