f129065a623478ca58b8e97505b9cc67bae317cc
Полина <pechenkova.polina.mgu@yandex.ru>
2024-12-09 06:44:41 +0300

    реализация авторизации через React Toolkit

--- pizza-app/src/helpers/RequireAuth.tsx
+++ pizza-app/src/helpers/RequireAuth.tsx
@@ -1,10 +1,12 @@
 import { ReactNode } from "react";
 import { Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { RootState } from "../store/store";
 
 export const RequireAuth = ({ children }: { children: ReactNode }) => {
-    const jwt = localStorage.getItem('jwt');
-    if (!jwt) {
-        return <Navigate to="/auth/login" replace />
-    }
-    return children;
+  const jwt = useSelector((s: RootState) => s.user.jwt);
+  if (!jwt) {
+    return <Navigate to="/auth/login" replace />;
+  }
+  return children;
 }
